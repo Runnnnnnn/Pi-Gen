0a15abfabb6c49ad7be7ad6fa1c8a026c3d956f2
Run <41304785+Runnnnnnn@users.noreply.github.com>
2018-07-16 16:08:17 -0400

    Update Pi_Generation.py

--- Pi_Generation.py
+++ Pi_Generation.py
@@ -1,12 +1,17 @@
 import numpy as np
 
+#number of simulation
 N = 10000000
 
+#generate a list of uniform numbers for x and y
 x = np.random.uniform(0,1,N)
 y = np.random.uniform(0,1,N)
 
+#count the number of points within the quarter circle(d=1)
 z = (x**2+y**2)**0.5
 quarter = np.where(z<=1,1,0)
+
+#calculate the value of pi
 pii = sum(quarter)*4/float(N)
 print(pii)
 
