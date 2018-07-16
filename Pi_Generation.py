d51c0df563c2a7a78b219b35b0de0fbd64dbf355
Run <41304785+Runnnnnnn@users.noreply.github.com>
2018-07-16 15:41:22 -0400

    Update Pi_Generation.py

--- Pi_Generation.py
+++ Pi_Generation.py
@@ -1,10 +1,13 @@
 import numpy as np
-x = np.random.uniform(0,1,10000000)
-y = np.random.uniform(0,1,10000000)
+
+N=10000000
+
+x = np.random.uniform(0,1,N)
+y = np.random.uniform(0,1,N)
 
 z = (x**2+y**2)**0.5
 quarter_fan = np.where(z<=1,1,0)
-pii = sum(quarter_fan)*4/float(len(x))
+pii = sum(quarter_fan)*4/float(N)
 print(pii)
 
 
