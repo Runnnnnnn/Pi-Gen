0d2751852d0db28ebb5095bbc24d1342dcb7d440
Run <41304785+Runnnnnnn@users.noreply.github.com>
2018-07-16 15:07:13 -0400

    Add files via upload

---
+++ Pi_Generation.py
@@ -0,0 +1,13 @@
+import numpy as np
+x = np.random.uniform(0,1,10000000)
+y = np.random.uniform(0,1,10000000)
+
+z = (x**2+y**2)**0.5
+quarter_fan = np.where(z<=1,1,0)
+pii = sum(quarter_fan)*4/float(len(x))
+print(pii)
+
+
+
+
+
