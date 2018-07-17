cafeb4d6870cf99645e8debeeb6c25e184e866e9
James Zhang <yuntao.james.zhang@gmail.com>
2018-07-17 15:41:23 -0700

    Update Pi_Generation.py
    This is my suggestions regarding OOP and your coding style. You can also add unit test class to see if the output meets your expectation.

--- Pi_Generation.py
+++ Pi_Generation.py
@@ -1,20 +1,63 @@
 import numpy as np
 
-#number of simulation
-N = 10000000
+class PiGeneration:
+    """
+    Summary of the class
+    
+    This class is designed to .....
+    
+    The attributes:
+    numberOfSimulation(int) - required - the number of simulation times
+    
+    The Method:
+    calc() - do the calculation
+    
+    The example of usage:
+    obj = PiGeneration(1000)
+    obj.calc()
+    
+    
+    Todo List:
+    1 - ...
+    2 - ...
+    3 - ...
+     
+    """
 
-#generate a list of uniform numbers for x and y
-x = np.random.uniform(0,1,N)
-y = np.random.uniform(0,1,N)
+    def __init__(self, N):
+        self.numberOfSimulation = N
 
-#count the number of points within the quarter circle(d=1)
-z = (x**2+y**2)**0.5
-quarter = np.where(z<=1,1,0)
+    def calc(self):
+        """
+        This method is desgined to...
+        It mainly has three parts:
+            1. generate a list of uniform numbers for x and y
+            2. count the number of points within the quarter circle(d=1)
+            3. calculate the value of pi
+        
+        Input parametr: 
+            None
+        
+        Returns:
+            calculated Pi
+        
+        """
+        #generate a list of uniform numbers for x and y
+        x = np.random.uniform(0,1,self.numberOfSimulation)
+        y = np.random.uniform(0,1,self.numberOfSimulation)
 
-#calculate the value of pi
-pii = sum(quarter)*4/float(N)
-print(pii)
+        #count the number of points within the quarter circle(d=1)
+        z = (x**2+y**2)**0.5
+        quarter = np.where(z<=1,1,0)
 
+        #calculate the value of pi
+        pii = sum(quarter)*4/float(self.numberOfSimulation)
+        return pii
+
+########### main function #####################
+if __name__ == '__main__':
+    obj = PiGeneration(1000)
+    print(obj.calc())
 
 
 
